a63214de7ae0d7832573151fe9f5ceb7bff079ac
margarinho <109757525+margarinho@users.noreply.github.com>
2025-02-10 21:58:24 +0000

    Implementação do extra
    O extra é uma filtragem por uma keyword (opcional)

--- tpc1/tpc1.py
+++ tpc1/tpc1.py
@@ -1,23 +1,25 @@
 import sys
 
-def reading_from_standrinput():
+def reading_from_stdin(keyword=None):
     uniqlines =  []
     while True:
         line = input("> ")
         if line not in uniqlines and not "":
-            uniqlines.append(line)
+            if keyword is None or keyword in line:
+                uniqlines.append(line)
         print ("\n".join(str(x) for x in uniqlines))
     
-def reading_from_file(filename):
+def reading_from_file(filename, keyword=None):
     uniqlines = []
     for line in open(filename, 'r').readlines():
         if line not in uniqlines:
-            uniqlines.append(line)
+            if keyword is None or keyword in line:
+                uniqlines.append(line)
     print("".join(str(x) for x in uniqlines))
 
 if __name__ == "__main__":
-    n = len(sys.argv)
-    if n == 1:
-        reading_from_standrinput()
+    keyword = sys.argv[2] if len(sys.argv) > 2 else None  # Palavra-chave opcional
+    if len(sys.argv) == 1:
+        reading_from_stdin(keyword)
     else:
-        reading_from_file(sys.argv[1])
+        reading_from_file(sys.argv[1], keyword)
